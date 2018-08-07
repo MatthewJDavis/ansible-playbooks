bed52570cbabe6bcae69235b113f902f7047dcff
= <matthewdavis111@gmail.com>
2018-08-06 22:17:51 -0400

    add winrm provisioner

---
+++ vagrant/provision-winrm.ps1
@@ -0,0 +1,7 @@
+# Configure winrm for ansible- test environments only
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+powershell.exe -ExecutionPolicy ByPass -File $file
