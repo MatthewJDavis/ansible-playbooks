e2bfa62d05d94189f28f00c77ca597be0177623e
= <matthewdavis111@gmail.com>
2018-08-06 22:13:24 -0400

    updated provision

--- vagrant/provision-ansible.sh
+++ vagrant/provision-ansible.sh
@@ -7,4 +7,7 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible -y
 sudo apt-get install git -y
+sudo apt-get install gcc python-dev libkrb5-dev
 sudo apt-get install python-pip -y
+sudo python -m pip install --upgrade pip setuptools
+pip install pywinrm --user
