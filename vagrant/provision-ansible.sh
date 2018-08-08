638ff20f311e59aafe00b055891909e7046cdfdd
= <matthewdavis111@gmail.com>
2018-08-07 20:29:23 -0400

    added apt upgrade step

--- vagrant/provision-ansible.sh
+++ vagrant/provision-ansible.sh
@@ -5,6 +5,7 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install ansible -y
 sudo apt-get install git -y
 sudo apt-get install gcc python-dev libkrb5-dev
