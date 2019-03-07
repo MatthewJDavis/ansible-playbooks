0a9e02dfeab4624e8016f9190c7ed5e9f41967c5
= <matthewdavis111@gmail.com>
2019-03-06 19:54:15 -0500

    added dsc config

--- ansible-kitchen/kitchen/vagrant_config_ansible.rb
+++ ansible-kitchen/kitchen/vagrant_config_ansible.rb
@@ -1,4 +1,5 @@
 Vagrant.configure("2") do |c|
+  c.winrm.username = 'vagrant\vagrant'
   c.vm.provision :shell do |shell|
     shell.path = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
   end
